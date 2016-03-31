f4af863d694c39dc04d7513d636938346fd8db25
sixpac009 <jpkeen01@gmail.com>
2016-03-31 13:05:42 -0400

    Corrected syntax in script

--- middleware/Log4j/log4j_install.sh
+++ middleware/Log4j/log4j_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script will install IBM Integration Bus (IIB)
-# v10.0.0.3
+# This script will install log4j node
+# 
 
 SCRIPT_DIR=${0%/*}
 SCRIPT_NAME=${0##*/}
@@ -9,61 +9,58 @@ exec > >(tee -a ${LOG_FILE} )
 exec 2> >(tee -a ${LOG_FILE} >&2)
 
 TMP_PATH='/tmp'
-INSTALL_PATH='/opt/IBM'
+INSTALL_PATH='/opt'
+IIB_PATH="$INSTALL_PATH/IBM"
 IIB_DIR='iib-10.0.0.3'
-IIB_TAR_NAME='10.0.0.3-IIB-LINUX64-DEVELOPER.tar.gz'
-echo $IIB_TAR_NAME
+L4J_DIR='log4j'
+L4J_TAR_NAME='iam3.zip'
 
 if [ "$EUID" -ne "0" ]; then
   echo "This script must be run as root."
   exit 1
 fi
 
-if [ -f "$INSTALL_PATH/$IIB_DIR/iib" ]; then
-  echo "IIB is already installed...."
+if [ -f "$IIB_PATH/$IIB_DIR/iib" ]; then
+  echo "IIB is required for log4j...."
+  exit 1
+fi
+
+if [ -f "$INSTALL_PATH/$L4J_DIR/IAM3.pdf" ]; then
+  echo "Log4j is already installed...."
   exit 0
 fi
 
 # Make sure wget is installed
-if yum list wget | grep Available; then
-  echo "Installing wget...."
-  yum -y install wget
-fi
-
-# Make sure webkitgtk is installed
-if yum list webkitgtk | grep Available; then
-  echo "Installing webkitgtk...."
-  yum -y install webkitgtk
+if yum list unzip | grep Available; then
+  echo "Installing unzip...."
+  yum -y install unzip
 fi
 
 # Create install directory
-if [ ! -d "$INSTALL_PATH" ]; then
+if [ ! -d "$INSTALL_PATH/$L4J_DIR" ]; then
   echo "Creating install directory...."
-  mkdir -p $INSTALL_PATH
+  mkdir -p $INSTALL_PATH/L4J_DIR
 fi
 
-# Extract tar file
+# Extract zip file
 if [ ! -d "$INSTALL_PATH/$IIB_DIR" ]; then
-  echo "Extractinr files from tar to install directory...."
-  tar -zxvf $SCRIPT_DIR/$IIB_TAR_NAME -C $INSTALL_PATH
+  echo "Extractinr files from zip to install directory...."
+  unzip $SCRIPT_DIR/$L4J_TAR_NAME -d $INSTALL_PATH/L4J_DIR
 fi
 
-# Add iib to path
-echo "export PATH=\$PATH:$INSTALL_PATH/$IIB_DIR" > /etc/profile.d/iib.sh
-echo "set path = (\$path $INSTALL_PATH/$IIB_DIR)" > /etc/profile.d/iib.csh
-source /etc/profile.d/iib.sh
+# Copy jar files to specified directories
+echo "Copy jar files to specified directories...."
+cd $INSTALL_PATH/$L4J_DIR
+cp Log4jLoggingNode_v1.2.2.jar jakarta-oro-2.0.4.jar log4j-1.2.8.jar /var/mqsi/shared-classes/.
+chmod 755 /var/mqsi/shared-classes/*.jar
+chown :mqbrkrs /var/mqsi/shared-classes/*.jar
 
-# Accept the IBM Integration Bus license
-echo "Accept the IBM Integration Bus license...."
-cd "$INSTALL_PATH/$IIB_DIR"
-./iib make registry global accept license silently
+cp Log4jLoggingNode_v1.2.2.jar $IIB_PATH/$IIB_DIR/server/jplugin/.
+chmod 755 $IIB_PATH/$IIB_DIR/server/jplugin/Log4jLoggingNode_v1.2.2.jar
+chown :mqbrkrs $IIB_PATH/$IIB_DIR/server/jplugin/Log4jLoggingNode_v1.2.2.jar
 
-# verify install version
-echo "Verify installed version...."
-./iib version
+unzip Log4jLoggingPluginFeature_v1.1.zip
 
-# add root to mqbrkrs group
-usermod -a -G mqbrkrs root
 
 echo "Install complete...."
 echo
